7dc52f64ce072b2ee435094c16406bb49263f2ac
Aniket Wandile <63203988+PyDeveloperAniket@users.noreply.github.com>
2024-06-19 10:05:42 +0530

    Click logic added

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log("Testing Github")
+console.log("Testing Github");
+let para = document.getElementById('para');
+para.addEventListener('click', function(){
+    document.getElementById('heading').style.color='green'
+})
